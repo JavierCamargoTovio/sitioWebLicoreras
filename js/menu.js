6038ba80daf08656e748d53264c78423119b8164
Javier Emilio Camargo Tovio <jcamargot@choucairtesting.com>
2020-05-17 09:29:18 -0500

    version 01-subiendo archivos

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+/*Codigo js para el boton menu*/
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnMenu.addEventListener('click', function() {
+    nav.classList.toggle('mostrar');
+
+});
